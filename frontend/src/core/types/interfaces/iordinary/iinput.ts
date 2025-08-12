7fb5558e1f5bcb47b20e55ea8d079c6c5cb6a219
Santas7 <andreypanyshkin03@gmail.com>
2025-08-12 12:34:22 +0400

    feat: modal window for creating a new project, and custom input, button components

---
+++ frontend/src/core/types/interfaces/iordinary/iinput.ts
@@ -0,0 +1,19 @@
+import type { ChangeEvent, HTMLInputTypeAttribute } from "react";
+
+export interface IInputProps {
+  id: string;
+  label: string;
+  type: HTMLInputTypeAttribute;
+  value: string | number;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
+}
+
+export interface IInputConfig {
+  id: string;
+  label: string;
+  type: 'text' | 'number';
+  value: string | number;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  placeholder: string;
+}
