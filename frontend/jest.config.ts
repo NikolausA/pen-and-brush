9eefd75ea892f9c77e6d57a0ba54f3080bb32d40
Santas7 <andreypanyshkin03@gmail.com>
2025-09-30 22:54:40 +0400

    базовый тест роутинга

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -4,12 +4,15 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/src/core/tests/setupTests.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^react-dnd$': '<rootDir>/src/core/tests/__mocks__/react-dnd.ts',
+    '^react-dnd-html5-backend$': '<rootDir>/src/core/tests/__mocks__/react-dnd-html5-backend.ts',
   },
   transform: {
     '^.+\\.(ts|tsx|js|jsx)$': ['ts-jest', {
       tsconfig: '<rootDir>/tsconfig.app.json',
-      diagnostics: true
-    }]
-  }
+      diagnostics: true,
+    }],
+  },
+  testMatch: ['<rootDir>/src/core/tests/**/*.test.tsx'], 
 };
---
+++ frontend/src/core/tests/__mocks__/react-dnd-html5-backend.ts
@@ -0,0 +1,3 @@
+import { jest } from '@jest/globals';
+
+export const HTML5Backend = jest.fn();
